fd5ed800438f6826a9799ec00a147c467c3f93f9
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-06-28 19:55:33 +0500

    ProfileRole and ProfileStatus added

---
+++ src/main/java/org/company/youtube/enums/ProfileRole.java
@@ -0,0 +1,6 @@
+package org.company.youtube.enums;
+
+public enum ProfileRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/org/company/youtube/enums/ProfileStatus.java
@@ -0,0 +1,7 @@
+package org.company.youtube.enums;
+
+public enum ProfileStatus {
+    ACTIVE,
+    NOT_ACTIVE,
+    REGISTRATION
+}
