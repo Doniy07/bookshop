88abb3b56e34f7ca935762818ee20d54ba84f782
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-06-28 20:03:04 +0500

    CustomUserDetailService added to project. It is used to load user from database

---
+++ src/main/java/org/company/youtube/config/CustomUserDetailService.java
@@ -0,0 +1,31 @@
+package org.company.youtube.config;
+
+
+
+import org.company.youtube.entity.profile.ProfileEntity;
+import org.company.youtube.repository.profile.ProfileRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private final ProfileRepository repository;
+
+    public CustomUserDetailService(ProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<ProfileEntity> optional = repository.findByEmail(email);
+        if (optional.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        ProfileEntity entity = optional.get();
+        return new CustomUserDetail(entity);
+    }
+}
