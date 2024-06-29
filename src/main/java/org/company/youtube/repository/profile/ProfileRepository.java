8b6ccbe09299e58d451c9c0a767de58bccb10928
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-06-29 14:43:01 +0500

    UpdateEmail method added to ProfileRepository

--- src/main/java/org/company/youtube/repository/profile/ProfileRepository.java
+++ src/main/java/org/company/youtube/repository/profile/ProfileRepository.java
@@ -16,4 +16,10 @@ public interface ProfileRepository extends CrudRepository<ProfileEntity, String>
     @Modifying
     @Query("update ProfileEntity set status =?2 where id =?1")
     void updateStatus(String profileId, ProfileStatus status);
-}
+
+    Optional<ProfileEntity> findByTempEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("update ProfileEntity set email =?2 where id =?1")
+    void updateEmail(String profileId, String email);}
