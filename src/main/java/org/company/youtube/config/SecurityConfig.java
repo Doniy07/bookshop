fc84965cd1515a232ad437146061089872554b9f
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-06-29 14:55:27 +0500

    SecurityConfig added

---
+++ src/main/java/org/company/youtube/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package org.company.youtube.config;
+
+
+
+import org.company.youtube.util.MD5Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@EnableWebSecurity
+@Component
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(customUserDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+
+                    .requestMatchers("/v2/api-docs").permitAll()
+                    .requestMatchers("/v3/api-docs").permitAll()
+                    .requestMatchers("/v3/api-docs/**").permitAll()
+                    .requestMatchers("/swagger-resources").permitAll()
+                    .requestMatchers("/swagger-resources/**").permitAll()
+                    .requestMatchers("/configuration/ui").permitAll()
+                    .requestMatchers("/configuration/security").permitAll()
+                    .requestMatchers("/swagger-ui/**").permitAll()
+                    .requestMatchers("/webjars/**").permitAll()
+                    .requestMatchers("/swagger-ui.html").permitAll()
+
+
+                    .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers("/adm/profile/create/*").hasRole("ADMIN")
+                    .requestMatchers("/profile/**").permitAll()
+                    .requestMatchers("/attach/**").permitAll()
+                    .anyRequest()
+                    .authenticated();
+        });
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return MD5Util.getMD5(rawPassword.toString()).equals(encodedPassword);
+            }
+        };
+    }
+
+}
