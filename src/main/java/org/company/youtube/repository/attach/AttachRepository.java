880b22ca98cc7961c3155cee2078943d860120c1
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-06-29 19:29:57 +0500

    AttachRepository remains unchanged

--- src/main/java/org/company/youtube/repository/attach/AttachRepository.java
+++ src/main/java/org/company/youtube/repository/attach/AttachRepository.java
@@ -2,7 +2,10 @@ package org.company.youtube.repository.attach;
 
 
 import org.company.youtube.entity.attach.AttachEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AttachRepository extends CrudRepository<AttachEntity, String> {
+public interface AttachRepository extends CrudRepository<AttachEntity, String>,
+        PagingAndSortingRepository<AttachEntity, String> {
 }
