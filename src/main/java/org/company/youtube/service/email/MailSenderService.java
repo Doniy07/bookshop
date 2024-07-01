5356dd1468f983bb9abab6f881c6efaa57a6c4e5
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-07-01 17:26:47 +0500

    MailSenderService RequiredArgsConstructor added

--- src/main/java/org/company/youtube/service/email/MailSenderService.java
+++ src/main/java/org/company/youtube/service/email/MailSenderService.java
@@ -2,18 +2,17 @@ package org.company.youtube.service.email;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MailSenderService {
-    private final JavaMailSender javaMailSender;
 
-    public MailSenderService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
+    private final JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
     private String fromEmail;
